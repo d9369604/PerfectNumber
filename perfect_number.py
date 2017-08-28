0abb1148417729212bc9e78afcffa6a573d73d0b
Gary Cheng <garycheng@kkbox.com>
2017-08-28 17:34:08 +0800

    modify the test case
    1. remove the specific input condition (ex: string)
    2. add the test cases of large number and zero

--- perfect_number.py
+++ perfect_number.py
@@ -1,12 +1,6 @@
 def is_perfect(number):
-
-    number = int(number)
-
-    if number <= 0:
-        raise Exception('please enter positive number')
-
     factor = [i for i in range(1, number) if not number % i]
-    return sum(factor) == number
+    return sum(factor) == number and number != 0
 
 if __name__ == '__main__':
     print is_perfect(6)
--- test_perfect_number.py
+++ test_perfect_number.py
@@ -9,17 +9,15 @@ class TestPerfectNumber(unittest.TestCase):
     def test_not_perfect_number(self):
         self.assertFalse(is_perfect(7))
 
-    def test_input_as_string(self):
-        self.assertTrue(is_perfect('6'))
-
     def test_number_one(self):
         self.assertFalse(is_perfect(1))
 
     def test_number_zero(self):
-        self.assertRaises(Exception, is_perfect, 0)
+        self.assertFalse(is_perfect(0))
 
-    def test_wrong_input_format(self):
-        self.assertRaises(ValueError, is_perfect, 'a')
+    def test_large_number(self):
+        self.assertTrue(is_perfect(33550336))
+
 
 if __name__ == '__main__':
     unittest.main()
